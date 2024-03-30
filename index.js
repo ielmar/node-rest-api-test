3832736a11dcc0f78f2fb40d7313272b611ada1b
Elmar Ismayilov <bamazy@gmail.com>
2024-03-30 22:50:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log("Server Listening on PORT:", PORT);
+});
+
+app.get("/status", (request, response) => {
+   const status = {
+      Status: "Running",
+   };
+
+   response.json(status);
+});
+
+app.post("/issue-otp", (request, response) => {
+   const status = {
+      status: "post request",
+   };
+   response.json(status);
+});
